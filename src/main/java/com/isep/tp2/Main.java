79419b9598de5b12155f901adee825ce1aadd276
minitableau <minitableau2002@gmail.com>
2023-02-14 15:03:50 +0100

    Tp 2 jusqu'à valeur des carrés

--- src/main/java/com/isep/tp2/Main.java
+++ src/main/java/com/isep/tp2/Main.java
@@ -6,9 +6,15 @@ import java.util.Scanner;
 //sout = System.out.println()
 public class Main {
     public static void main(String[] args) {
-        discriminant();
-        partie();
+//        discriminant();
+//        partie();
+//        max();
+//        min();
+//        egaliteStr();
+//        factorielle();
+        countdown();
     }
+
 
     public static void discriminant() {
         Scanner scanner = new Scanner(System.in);
@@ -42,10 +48,89 @@ public class Main {
         int a = scanner.nextInt();
         if (a % 2 == 0) {
             System.out.println("L'entier " + a + " est pair");
-        }
-        else{
+        } else {
             System.out.println("L'entier " + a + " est impair");
         }
     }
 
+    public static void max() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saisir deux entier");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if (a >= b) {
+            System.out.println("L'entier " + a + " est le maximum");
+//        } else if (a == b) {
+//            System.out.println("L'entier " + a + " est égal à l'entier " + b);
+        } else {
+            System.out.println("L'entier " + b + " est le maximum");
+        }
+    }
+
+    public static void min() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saisir deux entier");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if (a >= b) {
+            System.out.println("L'entier " + b + " est le minimum");
+        } else {
+            System.out.println("L'entier " + a + " est le minimum");
+        }
+    }
+
+    public static void egaliteStr() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saisir deux chaines de caractères");
+        String chaine1 = scanner.nextLine();
+        String chaine2 = scanner.nextLine();
+        if (chaine1.equals(chaine2)) {
+            System.out.println("Les chaines de caractères sont égales");
+        } else {
+            System.out.println("Les chaines de caractères ne sont pas égales");
+        }
+    }
+
+//salut
+//Bonjour
+//Les chaines de caractères ne sont pas égales
+
+//Bonjour
+//Bonjour
+//Les chaines de caractères sont égales
+
+//bonjour
+//Bonjour
+//Les chaines de caractères ne sont pas égales
+
+    public static void factorielle() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saisir un entier positif ou nul");
+        int n = scanner.nextInt();
+        int factorielle = 1;
+        for (int i = 1; i <= n; i++) {
+            factorielle *= i;
+        }
+        System.out.println(n + "! = " + factorielle);
+
+    }
+
+//    Question 2 - Il y a un problème avec la boucle sachant qu'elle commence à 0 la factorielle est donc forcément nulle
+//    Question 3 - Le programme est correct ex : 0! = 1 ; 3! = 6 ; 5! = 120
+
+    public static void countdown() {
+        int i = 11;
+        while (i != 0) {
+            i--;
+            System.out.println(i);
+        }
+        System.out.println("BOOM!");
+    }
+
+    public static void carres(){
+
+    }
 }
+
+
+
