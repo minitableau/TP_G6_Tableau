46d3e4476e5cea4dd0a2120761abe838ce0d973f
minitableau <minitableau2002@gmail.com>
2023-03-10 11:23:05 +0100

    ex piece explication et test pour certaines valeurs

--- src/main/java/com.isep.piece/Main.java
+++ src/main/java/com.isep.piece/Main.java
@@ -16,7 +16,9 @@ public class Main {
         int prix = 2;
 
 
-        int[] count = new int[prix + 1];
+        int[] count = new int[prix + 1]; // initalisation d'un tableau pour garder en memoire toutes
+        // les facons possibles de payer caque prix de 0 à prix.
+
         count[0] = 1; // si 0 prix 0 alors 1 possiblité pb !
 
         for (int i = 0; i < tableau.size(); i++) {
@@ -24,6 +26,12 @@ public class Main {
                 count[j] += count[j - tableau.get(i)];
             }
         }
+
+        //Pour toutes les pièces, on regarde tous les valeurs de j à prix (réduction des possiblités en diagonal)
+        //et pour chaque valeur j, on ajoute alors le nombre de façons différentes de payer j - tableau.get(i)
+        //avec la pièce actuelle à celui de j, ont stock ensuite le résultat dans le tableau à count[j].
+
+        // Pour obtenir le résultat final, il faut donc regarder la dernière case de notre tableau qui compte les possiblités.
 
         System.out.println("Il y'a " + count[prix] + " possibilité d'obtenir " + prix + " avec la liste suivante " + tableau);
     }
